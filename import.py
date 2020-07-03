ab6e82692cfa80c0f5206c781268075688547371
myusername <myemail>
2020-07-03 11:50:20 -0700

    web app

---
+++ import.py
@@ -0,0 +1,35 @@
+import os
+import csv
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError(" PLEASE SET THE DATABASE_URL")
+
+engine= create_engine(os.getenv("DATABASE_URL"))
+db =scoped_session(sessionmaker(bind=engine))
+
+def main():
+    db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, username VARCHAR NOT NULL, password VARCHAR NOT NULL)")
+    db.execute("CREATE TABLE reviews (isbn VARCHAR NOT NULL,review VARCHAR NOT NULL, rating INTEGER NOT NULL,username VARCHAR NOT NULL)")
+    db.execute("CREATE TABLE books (isbn VARCHAR PRIMARY KEY,title VARCHAR NOT NULL,author VARCHAR NOT NULL,year VARCHAR NOT NULL)")
+    f=open("books.csv")
+    reader =csv.reader(f)
+    for isbn,title,author,year in reader:
+        if year == "year":
+            print('THE DATA IS IMPORTING PLEASE WAIT')
+        else:
+            db.execute("INSERT INTO books (isbn, title, author, year) VALUES (:a,:b,:c,:d)",{"a":isbn,"b":title,"c":author,"d":year})
+
+    print("BOOKS.CSV DATA IS IMPORTED TO DATABASE")
+    db.commit()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
